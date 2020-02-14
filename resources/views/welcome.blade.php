4b3312aa58f5e2d78734ec82a440b96655a06370
gvincenta <gilbert11323@gmail.com>
2020-02-14 15:48:43 +1100

    backend routes now starts with /api/ to avoid route clashes with frontend. Changed the default Example.js to conventional App.js, and also handled CSRF token in the welcome.blade.php.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+ <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Books and Authors</title>
         <!-- Styles -->
-        <link href="/css/app.css" rel="stylesheet" type="text/css"/>
-
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div id="example"></div>
-        <script type="text/javascript" src="/js/app.js"> </script>
+        <div id="app"></div>
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
