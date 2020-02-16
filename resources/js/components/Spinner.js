90926ababd1b5539df539d107963f24ff3dc4eda
gvincenta <gilbert11323@gmail.com>
2020-02-16 14:00:23 +1100

    updated spinner's container

--- resources/js/components/Spinner.js
+++ resources/js/components/Spinner.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Spinner} from 'react-bootstrap';
+import {Spinner,ButtonToolbar} from 'react-bootstrap';
 //displays loading animation:
 export default function () {
     return (
-        <div className="container" align="center">
-            <Spinner animation="border" role="status">
-                <span align="center" className="sr-only">Loading...</span>
-            </Spinner>
+        <div>
+            <br/>
+            <ButtonToolbar className="mb-3" aria-label="Toolbar with Button groups">
+                <div className="container" align="center">
+                    <Spinner animation="border" role="status">
+                        <span align="center" className="sr-only">Loading...</span>
+                    </Spinner>
+                </div>
+            </ButtonToolbar>
+
         </div>
         );
 }
