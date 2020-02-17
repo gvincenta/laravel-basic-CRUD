9777e76feaed87fd3d76d8574618853d15c07799
gvincenta <gilbert11323@gmail.com>
2020-02-17 12:25:22 +1100

    completed export functionality

--- resources/js/components/Forms/Export.js
+++ resources/js/components/Forms/Export.js
@@ -7,146 +7,137 @@ import FileDownload from 'js-file-download';
 import { CSVLink, CSVDownload } from "react-csv";
 import { CsvToHtmlTable } from 'react-csv-to-table';
 
-///api/authors/export/CSV/with-books
+/** Handles exporting data to XML and CSV */
 export default function (props) {
+    //export to either XML or CSV:
     const [type,setType] = useState('');
+    //books / authors / authors and books content:
     const [content,setContent] = useState('');
-     const [status,setStatus] = useState('');
-    const [nesting,setNesting] = useState('');
+    //UI status:
+    const [status,setStatus] = useState('');
+    //data from backend:
     const [data, setData] = useState('');
+    //url for fetching data from backend:
     const [url,setURL] = useState('');
-    //for fetching data after forms are filled:
-    useLayoutEffect( () =>{
-            if (status=== "loading"){
-                Axios.get(url).then(
-                    (res)=>{
-                        console.log(res.data,"res");
-                        setData(res.data);
-                        setStatus("done");
 
-
-                    }
-                );
-            }
-
-        }
-        , [status] )
+    //displays loading spinner:
     if (status === "loading"){
         return <Spinner/>;
     }
+    //displays the xml with download option:
     if (status ==="done" && type === "xml"){
-
         return (
             <div>
                 <h2> Displaying {content} XML </h2>
                 <XMLViewer xml={data}/>
+                <a href="" onClick={(e)=>setStatus("download")}> Download me (in compact format) </a>
             </div>
         );
     }
+    //displays the csv file with download option:
     if (status ==="done" && type === "csv"){
         return (
             <div>
-            <h2> Displaying {content} CSV </h2>
-            <p>As a plain text :<br/>
+            <h2> Displaying {content} CSV as a plain text : </h2>
+
         <CsvToHtmlTable
         data={data}
-        csvDelimiter="," /> </p>
-            <p> As a table :
-            <br/>
-        <CsvToHtmlTable
-        data={data}
-        csvDelimiter=","
-        tableClassName="table table-striped table-hover"
-            /> </p>
+        csvDelimiter="," />
+
             <CSVLink data={data} >
             Download me
         </CSVLink>
             </div>
         ) ;
     }
+    //downloads the xml file:
     if (status==="download" && type === "xml"){
-        return FileDownload(data,"data.xml");
+        return FileDownload(data,content+".xml");
     }
 
 
-
+    //displays the form to choose what content and type is to be exported:
     return (
         <Form onSubmit =
         { (e) =>{
             e.preventDefault();
+            //sets URL properly:
             var url = "/api/" + content + "/export/" + type.toUpperCase()
+            if (content === "authors and books"){
+                 url = "/api/authors/export/" + type.toUpperCase() +"/with-books"
+            }
             console.log("URL", url);
             setURL(url);
 
-            if (content === "authorsAndBooks"){
-                if (type === "xml"){
-
-                }
-                else{
-
-                }
-            }
+            //for displaying spinner:
             setStatus("loading");
-
-
+            //fetching data from database:
+            Axios.get(url).then(
+                (res)=>{
+                    //data is ready:
+                    setData(res.data);
+                    setStatus("done");
+                }
+            );
         }}>
 
 
-    <fieldset>
-        <Form.Group as={Row}>
-            <Form.Label as="type" column sm={2}>
-                Export to:
-            </Form.Label>
-            <Col sm={10}>
-                <Form.Check
-                type="radio"
-                label="XML"
-                name="type"
-                id="xml"
-                onClick={e=>setType(e.target.id)}
-                required
-                />
-                <Form.Check
-                type="radio"
-                label="CSV"
-                name="type"
-                id="csv"
-                onClick={e=>setType(e.target.id)}
-                />
-            </Col>
-        </Form.Group>
-     </fieldset>
-
-     <fieldset>
-    <Form.Group as={Row}>
-    <Form.Label as="content" column sm={2}>
-        Radios
-        </Form.Label>
-        <Col sm={10}>
-        <Form.Check
-        type="radio"
-        label="books only"
-        name="content"
-        id="books"
-        onClick={e=>setContent(e.target.id)}
-        />
-        <Form.Check
-    type="radio"
-    label="authors only"
-    name="content"
-    id="authors"
-        />
-        <Form.Check
-    type="radio"
-    label="authors and books"
-    name="content"
-    id="authorsAndBooks"
-    required
-        />
-        </Col>
-        </Form.Group>
-        </fieldset>
-        <Button variant="primary" type="submit"> submit </Button>
+            <fieldset>
+                <Form.Group as={Row}>
+                    <Form.Label as="type" column sm={2}>
+                        Export to:
+                    </Form.Label>
+                    <Col>
+                        <Form.Check
+                        type="radio"
+                        label="XML"
+                        name="type"
+                        id="xml"
+                        onClick={e=>setType(e.target.id)}
+                        required
+                        />
+                        <Form.Check
+                        type="radio"
+                        label="CSV"
+                        name="type"
+                        id="csv"
+                        onClick={e=>setType(e.target.id)}
+                        />
+                    </Col>
+                </Form.Group>
+            </fieldset>
+            <fieldset>
+                <Form.Group as={Row}>
+                    <Form.Label as="content" column sm={2}>
+                        Radios
+                    </Form.Label>
+                    <Col>
+                        <Form.Check
+                        type="radio"
+                        label="books only"
+                        name="content"
+                        id="books"
+                        onClick={e=>setContent(e.target.id)}
+                        />
+                        <Form.Check
+                        type="radio"
+                        label="authors only"
+                        name="content"
+                        id="authors"
+                        onClick={e=>setContent(e.target.id)}
+                        />
+                        <Form.Check
+                        type="radio"
+                        label="authors and books"
+                        name="content"
+                        id="authors and books"
+                        onClick={e=>setContent(e.target.id)}
+                        required
+                        />
+                    </Col>
+                </Form.Group>
+            </fieldset>
+            <Button variant="primary" type="submit"> submit </Button>
         </Form>
     );
 
