298a634379858cc5679033cf54caf8572b480c43
gvincenta <gilbert11323@gmail.com>
2020-02-19 18:59:18 +1100

    created a BooksTest integration testing class: a class that tests most of /api/books endpoint. Inside the class, a generic test for getting Books and Authors table data as well as various inputs test for creating a book have been created.

---
+++ tests/Feature/BooksTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+//a class to test most of the /api/books endpoint
+class BooksTest extends TestCase
+{
+    //cleans up the DB before and after testing:
+    use RefreshDatabase;
+    // use without the need to send CSRF tokens to simplify http requests like post, put and delete.
+    use WithoutMiddleware;
+
+    /**
+     * @test tests a get request for Books And Authors table.
+     */
+    public function getBooksAndAuthors()
+    {
+        $response = $this->get('api/books');
+
+        $response->assertStatus(200);
+
+    }
+    /**
+     * @test adding a book and assigns authors to it.
+     */
+    public function addABookWithAuthor()
+    {
+        //testing for valid inputs:
+
+
+        $newAuthor = ['firstName' => 'Midoriya', 'lastName' => 'Zoldyck'];
+        $newAuthor2 = ['firstName' => 'Michael', 'lastName' => 'AJ'];
+        $response = $this->json('POST','/api/books',['title'=>'Alpha Beta',
+            'newAuthors' => [
+                $newAuthor,$newAuthor2
+            ]
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('books', ['title'=>'Alpha Beta']);
+        $this->assertDatabaseHas('authors', $newAuthor);
+        $this->assertDatabaseHas('authors', $newAuthor2);
+        //todo: make sure books connected to newly created authors:
+
+        //invalid input #1: non-string titles
+        $response = $this->json('POST','/api/books',['title'=>123,
+            'newAuthors' => [
+                $newAuthor
+            ]
+        ]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing ('books', ['title'=>123]);
+        //invalid input #2: empty titles
+        $response = $this->json('POST','/api/books',['title'=> "",
+            'newAuthors' => [
+                $newAuthor
+            ]
+        ]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing('books', ['title'=>'']);
+        //invalid input #3: no authors
+        $response = $this->json('POST','/api/books',['title'=> "No Authors"]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing('books', ['title'=>'No Authors']);
+        //invalid input #4: new authors exist with no body
+        $response = $this->json('POST','/api/books',['title'=> "new authors exist with no body",
+            "newAuthors" =>  []]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing('books', ['title'=>'new authors exist with no body']);
+        //invalid input #5: existing authors exist with no body
+        $response = $this->json('POST','/api/books',['title'=> "existing authors exist with no body",
+            "authors" =>  []]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing('books', ['title'=>'existing authors exist with no body']);
+        //invalid input #6: new authors exist with improper body
+        $response = $this->json('POST','/api/books',['title'=> "new authors exist with improper body",
+            "newAuthors" =>  ['firstName' =>123, 'lastName'=>456]]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing('books', ['title'=>'new authors exist with improper body']);
+        //invalid input #7: existing authors exist with improper body
+        $response = $this->json('POST','/api/books',['title'=> "existing authors exist with improper body",
+            "authors" =>  ['ID' => '1']]);
+        $response->assertStatus(400);
+        $this->assertDatabaseMissing('books', ['title'=>'existing authors exist with improper body']);
+    }
+
+
+}
