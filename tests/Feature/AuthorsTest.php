04337977db829bbda695daa11f541501c5f76144
gvincenta <gilbert11323@gmail.com>
2020-02-20 12:27:02 +1100

    added test cases to changing an author's name functionality

--- tests/Feature/AuthorsTest.php
+++ tests/Feature/AuthorsTest.php
@@ -24,6 +24,12 @@ class AuthorsTest extends TestCase
         $createResponse->assertStatus(201);
         $this->assertTrue(gettype($createResponse['newAuthorsID'][0]) == "integer");
         $this->assertDatabaseHas('authors', $newAuthor);
+        //BUG: try update with an invalid ID :
+//        $updateResponse = $this->json('PUT','/api/authors',['ID'=>strval($createResponse['newAuthorsID'][0]),
+//            'firstName' => 'Updated',
+//            'lastName' =>'Updated'
+//        ]);
+//        $updateResponse->assertStatus(400);
         //then, update an author it through its ID:
         $updateResponse = $this->json('PUT','/api/authors',['ID'=>$createResponse['newAuthorsID'][0],
             'firstName' => 'Updated',
@@ -32,8 +38,29 @@ class AuthorsTest extends TestCase
         $this->assertDatabaseHas('authors', ['firstName' => 'Updated',
             'lastName' =>'Updated']);
         $this->assertDatabaseMissing('authors', $newAuthor);
-
-
+        //update with empty body:
+        $updateResponse = $this->json('PUT','/api/authors',[]);
+        $updateResponse->assertStatus(400);
+        //update with ID only:
+        $updateResponse = $this->json('PUT','/api/authors',['ID'=>$createResponse['newAuthorsID'][0]  ]);
+        $updateResponse->assertStatus(400);
+        //update with invalid firstName and lastName:
+        $updateResponse = $this->json('PUT','/api/authors',['ID'=>$createResponse['newAuthorsID'][0],
+        'firstName' => 123, 'lastName' => 456]);
+        $updateResponse->assertStatus(400);
+        //update with invalid firstName:
+        $updateResponse = $this->json('PUT','/api/authors',['ID'=>$createResponse['newAuthorsID'][0],
+            'firstName' => 123, 'lastName' => "John"]);
+        $updateResponse->assertStatus(400);
+        //update with invalid lastName:
+        $updateResponse = $this->json('PUT','/api/authors',['ID'=>$createResponse['newAuthorsID'][0],
+            'firstName' => "John", 'lastName' => 456]);
+        $updateResponse->assertStatus(400);
+        //update with firstName and lastName only:
+        $updateResponse = $this->json('PUT','/api/authors',[$newAuthor]);
+        $updateResponse->assertStatus(400);
     }
+    
+
 
 }
