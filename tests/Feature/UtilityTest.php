9b6ef21fb3368ea2425c8ec57cc838f34be48953
gvincenta <gilbert11323@gmail.com>
2020-02-21 20:19:54 +1100

    added comments to (most of) UtilityTest functions

--- tests/Feature/UtilityTest.php
+++ tests/Feature/UtilityTest.php
@@ -6,17 +6,39 @@ use Maatwebsite\Excel\Facades\Excel;
 use Tests\TestCase;
 use App\Exports\DBExport;
 
-
+/**
+ * Class UtilityTest
+ * @package Tests\Feature\
+ * used to make helper functions to be used by other feature testing classes.
+ * TODO: be more specific on the XML testing method?
+ */
 class UtilityTest extends TestCase
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        parent::setUp();
+    }
+    /** Used to post to /api/books route to make a new book and assign authors to it.
+     * @param $title required to specify the new book's title.
+     * @param array $newAuthor to assign non-existing authors to this new book. (optional)
+     * @param array $existingAuthors to assign existing authors (in database) to this new book. (optional)
+     * @return mixed the response from the route.
+     */
     public function createABook($title, $newAuthor = [], $existingAuthors = [] ){
         return   $this->json('POST','/api/books',['title'=>$title,
             'newAuthors' => $newAuthor,
             'authors' => $existingAuthors
         ]);
     }
-    public function checkJsonContent($searchResponse, $src){
-        $searchResponse
+
+    /** checks whether a response returns a certain json structure.
+     * @param $sresponse  the response to be checked against.
+     * @param $src the expected value that the response should contain.
+     */
+    public function checkJsonContent($sresponse, $src){
+        $sresponse
             ->assertStatus(200)
             ->assertExactJson( [[
                 "ID"=> $src['authorID'],
@@ -27,19 +49,19 @@ class UtilityTest extends TestCase
             ] ]);
 
     }
-    public function checkEmptyJsonContent($searchResponse){
-        $searchResponse
+
+    /** checks for empty json response.
+     * @param $response the response from backend to be checked.
+     */
+    public function checkEmptyJsonContent($response){
+        $response
             ->assertStatus(200)
             ->assertExactJson( []);
     }
-    public function __construct()
-    {
-        parent::__construct();
-        parent::setUp();
-    }
+
+
     /** Tests whether an exported CSV file is empty or not.
-     *
-     * @param $url specifies where to get the CSV file.
+     * @param $url specifies where to get the CSV file from.
      * */
     public function exportEmptyCSV($url,$fileName){
         Excel::fake();
@@ -54,10 +76,9 @@ class UtilityTest extends TestCase
         });
     }
     /** Tests an exported CSV file according to the headers that need to be checked against.
-     *
      * @param $headersToBeChecked the headers to be checked against the CSV file
      * @param $url specifies where to get the CSV file.
-     * @param $fileName the name of the CSV file that is downloaded.
+     * @param $fileName the name of the CSV file that is (assumed to be) downloaded.
      * */
     public function exportToCSV($headersToBeChecked, $url, $fileName){
         $this->exportEmptyCSV($url,$fileName);
@@ -96,18 +117,26 @@ class UtilityTest extends TestCase
             return true;
         });
     }
-    //search for a book a valid title / firstName and lastName:
+    /** testing the search functionality with a valid title / author.
+     * @param $url specifies where to send the search request to.
+     * @param $src contains the search keyword (title, firstName and lastName).
+     */
     public function searchWithValidRequest($url,$src){
         $searchResponse = $this->json('GET',$url,[
             'title'=>$src["title"], //for a search by title
             'firstName' =>$src['firstName'] , //for a search by author
-            'lastName' => $src['lastName']
+            'lastName' => $src['lastName'] //for a search by author
         ]);
 
         //check the response:
         $this->checkJsonContent($searchResponse,$src);
     }
-    //make sure only return exact matches by sending non-exact requests.
+    //
+
+    /** make sure search for a book only works with  exact matches. this is tested by sending non-exact match requests.
+     * @param $url the route to send the request to.
+     * @param $src the source that has the exact matching values for the book's title or author.
+     */
     public function searchExactMatchOnly($url,$src){
         $searchResponse = $this->json('GET',$url,['title'=>$src["title"][0],
             'firstName' =>$src['firstName'][0],
@@ -117,7 +146,7 @@ class UtilityTest extends TestCase
         $this->checkEmptyJsonContent($searchResponse);
     }
 
-    /** Conducts searching test by an author or a book's title:
+    /** Conducts tests for searching  by an author or a book's title:
      * 1. tests for with valid request
      * 2. tests with non-exact request
      * 3. tests with invalid request
@@ -145,9 +174,22 @@ class UtilityTest extends TestCase
         //expect for status: 400 response:
         $this->checkInvalidResponse($searchResponse);
     }
+
+    /** Sends a request with no json body to a specific route.
+     * @param $url, the url to send the request to.
+     * @param $verb, the HTML Verb (POST, PUT, DELETE, GET).
+     * @return mixed, the response from backend.
+     */
     public function sendEmptyRequest($url,$verb){
         return $this->json($verb,$url, []);
     }
+
+    /**
+     * @param $headersToBeChecked
+     * @param $url
+     * @param $rootTag
+     * @param string $nestedTag
+     */
     public function exportToXML($headersToBeChecked, $url, $rootTag ,$nestedTag=""){
         //firstly, must create a book with an author:
         $newAuthor = ['firstName' => 'Midoriya', 'lastName' => 'Zoldyck'];
@@ -190,7 +232,16 @@ class UtilityTest extends TestCase
        $this->checkNestedXML($headersToBeChecked,$childArray,$src,$nestedTag,$object->data,$rootTag);
 
     }
-    //check if the xml has a nested element, e.g. books under authors or vice versa:
+    //
+
+    /** check if the xml has a nested element, e.g. books under authors or vice versa:
+     * @param $headersToBeChecked, contains the headers to be checked against the XML file.
+     * @param $childArray, the
+     * @param $src, contains the expected values of each header to be checked against the XML file.
+     * @param $nestedTag, the tag that indicates the XML has a nested element that needs to be checked again.
+     * @param $xml, the xml file to be checked against.
+     * @param $rootTag, authors OR books.
+     */
     public function checkNestedXML($headersToBeChecked,$childArray,$src,$nestedTag, $xml,$rootTag){
         //if no nesting, just loop:
         $this->validateXMLContent($headersToBeChecked[0],$childArray,$src,$rootTag);
@@ -206,7 +257,13 @@ class UtilityTest extends TestCase
 
         }
     }
-    //check that current xml elements under <data> are valid:
+
+    /** check that current xml elements under <data> are valid:
+     * @param $headersToBeChecked the headers to be checked against in the XML file.
+     * @param $childArray
+     * @param $src , contains the expected values of each header to be checked against the XML file.
+     * @param $currentTag authors OR books.
+     */
     public function validateXMLContent($headersToBeChecked,$childArray,$src,$currentTag){
         foreach ($headersToBeChecked as $header) {
             if ($currentTag == "authors" && $header=="ID"){
@@ -218,12 +275,21 @@ class UtilityTest extends TestCase
             }
         }
     }
-    //when you send an invalid request, the backend returns an "invalid request" message. check for this kind of message here.
+    /** When you send an invalid request, the backend returns an "invalid request" message. check for this kind of
+     * response here.
+     * @param $response the response from backend to be checked against.
+     */
     public function checkInvalidResponse($response){
         $response
             ->assertStatus(400)
             ->assertExactJson(["message" => "invalid request"]);
     }
+
+    /** When you send a valid requests, backend may repsonse with OK status with a customised message. check for this
+     * kind of message here.
+     * @param $response the response from backend to be checked against.
+     * @param $message the customised message expected from backend (usually a success / failure message).
+     */
     public function checkOKResponseWithCustomMessage($response, $message){
         $response
             ->assertStatus(200)
