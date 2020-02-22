d64531eff2d1394b7ef8dd06ae6bbc00f4254481
gvincenta <gilbert11323@gmail.com>
2020-02-22 21:18:15 +1100

    added comments to DBExport class

--- app/Exports/DBExport.php
+++ app/Exports/DBExport.php
@@ -6,8 +6,12 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-
-   class DBExport implements FromCollection,WithHeadings
+/**
+ * Class DBExport
+ * @package App\Exports
+ * An adapter class that communicates with Maatwebsite\Excel\... components when exporting data to CSV.
+ */
+class DBExport implements FromCollection,WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -15,15 +19,17 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
     private $headings =[];
     private $exportData = []; //array of json as extracted from database.
 
-    //by default, export data and set headings directly.
+       /** by default, set export data and set headings directly.
+        * DBExport constructor.
+        * @param $exportData
+        * @param $headings
+        */
        public function __construct($exportData,$headings)
        {
-           //extract columns from query:
+           //get data headings from query:
            $this->setHeadings( $headings );
            //get data collection from query:
-           $this->setExportData(
-               $exportData
-           );
+           $this->setExportData(  $exportData );
        }
 
      public function collection()
@@ -32,21 +38,32 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
     }
 
 
-    //implements WithHeadings interface:
+       /** implements WithHeadings interface.
+        * @return array, the headings / columns to be exported to CSV file.
+        */
     public function headings():array
     {
         return $this->headings;
     }
-    //setter for headings:
+
+       /** setter for headings.
+        * @param $headings
+        */
     public function setHeadings($headings )
     {
         $this->headings = $headings;
     }
 
-     /* getter setter for exportData: */
+       /** getter for exportData.
+        * @return array, the data to be exported.
+        */
      public function getExportData(){
         return $this->exportData;
      }
+
+       /** setter for exportData.
+        * @param $exportData
+        */
      public function setExportData($exportData){
         $this->exportData = $exportData;
      }
