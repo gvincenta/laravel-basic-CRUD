6ca7fe46d2cb25f806f377d91c10ee56186c5e3a
gvincenta <gilbert11323@gmail.com>
2020-02-22 15:53:16 +1100

    fixed import issues

--- database/migrations/2020_02_12_010705_create_authors_books_table.php
+++ database/migrations/2020_02_12_010705_create_authors_books_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Books;
+use App\Authors;
 
 class CreateAuthorsBooksTable extends Migration
 {
