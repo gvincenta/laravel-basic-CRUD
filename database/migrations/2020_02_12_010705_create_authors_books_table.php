58f684a8fb07cb01dfbebaf648acab958928e986
gvincenta <gilbert11323@gmail.com>
2020-02-23 13:26:13 +1100

    added "authorship " index: do not allow duplicate (authorID,bookID) entries in pivot table in backend.

--- database/migrations/2020_02_12_010705_create_authors_books_table.php
+++ database/migrations/2020_02_12_010705_create_authors_books_table.php
@@ -18,14 +18,22 @@ class CreateAuthorsBooksTable extends Migration
 
         Schema::create(PivotController::TABLE_NAME, function (Blueprint $table) {
             $table->increments(PivotController::ID_FIELD);
+            //allow books to have many authors and vice versa:
              $table->integer(PivotController::AUTHORS_ID_FIELD)->unsigned();
             $table->integer(PivotController::BOOKS_ID_FIELD)->unsigned();
+            //allow books to be deleted and updated:
             $table->foreign( PivotController::BOOKS_ID_FIELD)
                 ->references(Books::ID_FIELD)->on(Books::TABLE_NAME)
                 ->onDelete('cascade')->onUpdate('cascade');
+            //don't delete an author, and when their ID is updated, apply it to the whole table:
             $table->foreign(PivotController::AUTHORS_ID_FIELD)
                 ->references(Authors::ID_FIELD)->on(Authors::TABLE_NAME)
                 ->onDelete('restrict')->onUpdate('cascade');
+            //allow only 1 entry for each (authorID, bookID) pair:
+            $table->unique([
+                PivotController::AUTHORS_ID_FIELD,
+                PivotController::BOOKS_ID_FIELD
+            ], 'Authorship');
 
         });
     }
